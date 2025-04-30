fbcf5dca96d171795ad967cd257b8837cc51fd24
mistery5677 <miguel.costa.5677@gmail.com>
2025-04-30 11:19:15 +0100

    Updated CPP07

--- CPP07/ex01/include/iter.hpp
+++ CPP07/ex01/include/iter.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mistery576 <mistery576@student.42.fr>      +#+  +:+       +#+        */
+/*   By: miafonso <miafonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 21:46:30 by mistery576        #+#    #+#             */
-/*   Updated: 2025/04/23 21:46:31 by mistery576       ###   ########.fr       */
+/*   Updated: 2025/04/30 11:19:04 by miafonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@
 template <typename X, typename Y, typename Func> void iter(X *arr, Y lenght, Func func){
     if (!arr)
         return ;
-    for (int i = 0; arr[i] && i < lenght; i++)
-        func(arr[i]);
+    for (int i = 0; arr + i && i < lenght; i++)
+        func(*(arr + i));
 }
